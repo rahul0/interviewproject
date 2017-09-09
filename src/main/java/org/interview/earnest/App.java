7b4260bd55fcb652c1c274ff3b3e598dff62a3b0
rahulkumar <RahulKumar2@thomsonreuters.com>
2017-09-08 21:47:07 -0700

    setting up

--- src/main/java/org/interview/earnest/App.java
+++ src/main/java/org/interview/earnest/App.java
@@ -1,13 +1,22 @@
 package org.interview.earnest;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    final static Logger logger = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.info("info message");
+        logger.info("info message");
+        logger.info("info message");
+        logger.info("info message");
+        logger.info("info message");
+        System.out.println( "Hello World1!" );
     }
 }
